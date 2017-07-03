ad7dd7832f32640fe1f6d445fdc34fb4ad957dc1
Bryce <bryce.sayers-kwan@live.ca>
2017-07-03 22:05:48 +0000

    Started a function to get repo contributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  request.get('https://github.com/' + repoOwner + "/" + repoName);
+}
+
+getRepoContributors('nodejs', 'node', function(err, result) {
+  console.log("Errors:", err);
+  console.log("Results:", results);
+});
