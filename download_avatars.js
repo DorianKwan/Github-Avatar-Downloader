221e48816fe564368c3b77f7b26fb0dd07097cd8
Bryce <bryce.sayers-kwan@live.ca>
2017-07-04 00:15:43 +0000

    Added a function to return an object with url and github token within & called the original function to log all the contributors avatar

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,33 @@
 const request = require('request');
-
+// Log a welcome message to the user
 console.log('Welcome to the GitHub Avatar Downloader!');
-
-function getRepoContributors(repoOwner, repoName, cb) {
-  request.get('https://github.com/' + repoOwner + "/" + repoName);
+// Function to create request options
+function getRequestOptions(path) {
+  return {
+    url: 'https://api.github.com/repos/' + path + '/contributors',
+    headers: {
+      'User-Agent': 'wizard'
+    },
+    qs: {
+      access_token: process.env.GITHUB_ACCESS_TOKEN
+    }
+  };
 }
-
-getRepoContributors('nodejs', 'node', function(err, result) {
-  console.log("Errors:", err);
-  console.log("Results:", results);
+// Function to get the contributors from a given repo
+function getRepoContributors(repoOwner, repoName, cb) {
+  const path = repoOwner + '/' + repoName;
+  request(getRequestOptions(path), function (error, response, body) {
+    try {
+      const data = JSON.parse(body);
+      cb(data);
+    } catch (err) {
+      console.log('Failed to parse content body')
+    }
+  });
+}
+// Invoke the function with arguments
+getRepoContributors('nodejs', 'node', (data) => {
+  data.forEach((contributor) => {
+    console.log(contributor.avatar_url);
+  })
 });
